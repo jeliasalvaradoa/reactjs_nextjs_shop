55c31f60ddf8e1dc2a138af1756c9cbe163091d2
jeliasalvaradoa <josealvarado30@hotmail.com>
2023-02-16 01:31:07 -0400

    [fix] corrigiendo bugs

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  pwa:{
-    dest: 'public',
-    register: true,
-    mode: 'production',
-    disable: false,
-  },
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space', 'i.imgur.com', 'raw.githubusercontent.com', 'cdn.shopify.com'],
-  },
+   },
+};
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
 });
+
+module.exports = withPWA(nextConfig);
