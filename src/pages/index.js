e48aeef21e0757f10772a2303e1956435838c3a7
Jose E. Alvarado A <103224278+jeliasalvaradoa@users.noreply.github.com>
2023-11-23 01:54:39 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import ProductList from '@containers/ProductList';
 import Head from 'next/head';
-export default function Home() {
+export default function Home({categoryId}) {
   return (
     <>
       <Head>
         <title>React Shop</title>
       </Head>
-      <ProductList />
+      <ProductList categoryId />
     </>
   );
 }
